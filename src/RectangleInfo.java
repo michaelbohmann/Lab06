f18418fa7e29c18f0b4fd274cede56fa28567a8c
michaelbohmann <bohmanmh@mail.uc.edu>
2024-10-14 20:23:46 -0400

    final commit

--- src/RectangleInfo.java
+++ src/RectangleInfo.java
@@ -1,2 +1,58 @@
-public class RectangleInfo {
+import java.util.Scanner;
+
+public class RectangleInfo
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        double width = 0, height = 0;
+        String trash = "";
+        boolean done = false;
+
+
+        do
+        {
+            System.out.print("Enter the width of the rectangle: ");
+            if (in.hasNextDouble())
+            {
+                width = in.nextDouble();
+                in.nextLine();
+                done = true;
+            }
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("Invalid input: " + trash);
+            }
+        }
+        while (!done);
+
+        done = false;
+
+
+        do
+        {
+            System.out.print("Enter the height of the rectangle: ");
+            if (in.hasNextDouble()) {
+                height = in.nextDouble();
+                in.nextLine();
+                done = true;
+            }
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("Invalid input: " + trash);
+            }
+        }
+        while (!done);
+
+
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+        double diagonal = Math.sqrt((width * width) + (height * height));
+
+        System.out.println("Area: " + area);
+        System.out.println("Perimeter: " + perimeter);
+        System.out.println("Diagonal: " + diagonal);
+    }
 }
