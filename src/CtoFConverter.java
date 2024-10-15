5cefd673bac39ac2996fe4e4585d6c254d77568f
michaelbohmann <bohmanmh@mail.uc.edu>
2024-10-14 20:25:27 -0400

    final commit

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,6 +1,34 @@
-public class CtoFConverter
+import java.util.Scanner;
+
+class CtoFConverter
 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        double celsius = 0;
+        String trash = "";
+        boolean done = false;
+
+
+
+        do
+        {
+            System.out.print("Enter temperature in Celsius: ");
+            if (in.hasNextDouble()) {
+                celsius = in.nextDouble();
+                in.nextLine();
+                done = true;
+            }
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("Invalid input: " + trash);
+                System.out.println("Please enter a valid temperature.");
+            }
+        } while (!done);
+
+
+        double fahrenheit = (9.0 / 5.0) * celsius + 32;
+        System.out.println("The equivalent temperature in Fahrenheit is: " + fahrenheit);
     }
 }
